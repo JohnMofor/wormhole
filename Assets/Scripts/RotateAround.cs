2ef8bb0e213b2d6a0294bd989519adb859397b15
John Mofor <moforj@gmail.com>
2015-04-30 14:47:26 -0400

    Adding scene files and co

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+
+	public float angle = 10f;
+	public Vector3 center = Vector3.zero;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(center, Vector3.up, angle * Time.deltaTime);
+	}
+}
